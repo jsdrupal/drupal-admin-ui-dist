b1b921e4c332ee6b4766151a92a58ea0e29d237f
⬅️✌️➡️ <no-reply@drupal.org>
2018-12-10 15:41:01 +0000

    Auto build from master

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vfancy/precache-manifest.5b0da2ea7822f20d0db278d8e25b3c1a.js"
+  "/vfancy/precache-manifest.1c49a0518873f8f5f91cb43ee96ee0a6.js"
 );
 
 workbox.clientsClaim();
