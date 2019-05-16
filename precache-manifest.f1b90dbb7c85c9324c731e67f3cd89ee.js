15ecae8834a3b140d44f676c836eed7efa5aae2b
⬅️✌️➡️ <no-reply@drupal.org>
2019-05-16 14:43:03 +0000

    Auto build from master

---
+++ precache-manifest.f1b90dbb7c85c9324c731e67f3cd89ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e907904b5ad38c29032610b467d65b",
+    "url": "/vfancy/index.html"
+  },
+  {
+    "revision": "c1945c724d59fc57764b",
+    "url": "/vfancy/static/js/2.9cc487aa.chunk.js"
+  },
+  {
+    "revision": "da3af3b4c9d3fb8a8946",
+    "url": "/vfancy/static/js/main.46fd9624.chunk.js"
+  },
+  {
+    "revision": "7e6292734d3aa492d020",
+    "url": "/vfancy/static/js/runtime~main.d0463fc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vfancy/precache-manifest.97c752979d94af6a0b5306d7c8fafcae.js"
+  "/vfancy/precache-manifest.f1b90dbb7c85c9324c731e67f3cd89ee.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vfancy/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vfancy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
